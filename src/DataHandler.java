4a1178f12063881da61a5a0ccacca8240d7f1501
rafalesso1991 <rafalesso1991@gmail.com>
2025-09-15 00:01:29 -0300

    agregue clase DataHandler y agregué manejo de ArrayList

---
+++ src/DataHandler.java
@@ -0,0 +1,68 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class DataHandler {
+
+    public static void guardarTodosLosPersonajes(List<Personaje> personajes, String nombreArchivo) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(nombreArchivo, false))) { // Using false to overwrite the file
+            for (Personaje personaje : personajes) {
+                writer.println(personaje.getNombre() + "," + personaje.getRaza().toString() + "," + personaje.getRol().toString() + "," +
+                        personaje.getFuerza() + "," + personaje.getAgilidad() + "," + personaje.getInteligencia() + "," + personaje.getVoluntad());
+            }
+            System.out.println("Todos los personajes guardados en " + nombreArchivo);
+        } catch (IOException e) {
+            System.out.println("Ocurrio un error al guardar los personajes.");
+        }
+    }
+
+    public static List<Personaje> cargarTodosLosPersonajes(String nombreArchivo) {
+
+        List<Personaje> personajesCargados = new ArrayList<>();
+        File file = new File(nombreArchivo);
+
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(",");
+                if (parts.length == 7) {
+                    String nombre = parts[0];
+                    Raza raza = Raza.valueOf(parts[1]);
+                    Rol rol = Rol.valueOf(parts[2]);
+                    int fuerza = Integer.parseInt(parts[3]);
+                    int agilidad = Integer.parseInt(parts[4]);
+                    int inteligencia = Integer.parseInt(parts[5]);
+                    int voluntad = Integer.parseInt(parts[6]);
+
+                    // Crea una instancia de la clase de personaje específica según la profesión elegida.
+                    switch (rol) {
+                        case GUERRERO:
+                            personajesCargados.add(new Guerrero(nombre, raza, fuerza, agilidad, inteligencia, voluntad));
+                            break;
+                        case MAGO:
+                            personajesCargados.add(new Mago(nombre, raza, fuerza, agilidad, inteligencia, voluntad));
+                            break;
+                        case LADRON:
+                            personajesCargados.add(new Ladron(nombre, raza, fuerza, agilidad, inteligencia, voluntad));
+                            break;
+                        case SACERDOTE:
+                            personajesCargados.add(new Sacerdote(nombre, raza, fuerza, agilidad, inteligencia, voluntad));
+                            break;
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Archivo no encontrado: " + nombreArchivo);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error al analizar los datos del archivo. El archivo podría estar dañado.");
+
+            return new ArrayList<>();
+        }
+        return personajesCargados;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 // MAIN CLASS --> Maneja las entradas y la creación del personaje.
@@ -7,156 +8,187 @@ public class Main {
         // Crea un objeto Scanner para leer la entrada del usuario desde la consola.
         Scanner scanner = new Scanner(System.in);
 
-        // Prompt NOMBRE
-        System.out.print("Ingresa el nombre de tu personaje: ");
-        String nombre = scanner.nextLine();
+        // --- Manejo de ArrayList ---
+        // Esta ArrayList almacenará todos los personajes creados durante la sesión.
+        List<Personaje> grupoDePersonajes = new ArrayList<>();
 
-        // Prompt RAZA
-        Raza raza = null;
-        while (raza == null) {
-            System.out.println("Elije una raza (Humano, Elfo, Enano, Orco): ");
-            String razaIngresada = scanner.nextLine().toUpperCase();
-            try {
-                // Convierte el String en una constante "enum"
-                raza = Raza.valueOf(razaIngresada);
-            } catch (IllegalArgumentException e) {
-                System.out.println("Raza inválida. Por favor, seleccione una de la lista.");
+
+        // Bucle del programa principal
+        while (true) {
+            System.out.println("\n--- MENU PRINCIPAL ---");
+            System.out.println("1. Crear un nuevo personaje");
+            System.out.println("2. Ver to.dos los personajes");
+            System.out.println("3. Selecciona a un personaje");
+            System.out.println("4. Guardar todos los personajes en un archivo");
+            System.out.println("5. Cargar personajes del archivo");
+            System.out.println("6. Salir");
+            System.out.print("Elige tu opción: ");
+            String opcionMenuPrincipal = scanner.nextLine();
+
+            // --- ESTRUCTURA DEL PROGRAMA: Declaraciones condicionales ---
+            switch (opcionMenuPrincipal) {
+                case "1":
+                    // --- CREACION DE PERSONAJE ---
+                    System.out.print("Ingresa el nombre de tu personaje: ");
+                    String nombre = scanner.nextLine();
+
+                    Raza raza = null;
+                    while (raza == null) {
+                        System.out.println("Elije una raza (Humano, Elfo, Enano, Orco): ");
+                        String razaIngresada = scanner.nextLine().toUpperCase();
+                        try {
+                            raza = Raza.valueOf(razaIngresada);
+                        } catch (IllegalArgumentException e) {
+                            System.out.println("Raza inválida. Seleccione una de la lista.");
+                        }
+                    }
+
+                    Rol rol = null;
+                    while (rol == null) {
+                        System.out.println("Elije un rol (Guerrero, Mago, Ladron, Sacerdote): ");
+                        String rolIngresado = scanner.nextLine().toUpperCase();
+                        try {
+                            rol = Rol.valueOf(rolIngresado);
+                        } catch (IllegalArgumentException e) {
+                            System.out.println("Rol inválido, Seleccione uno de la lista.");
+                        }
+                    }
+
+                    int fuerza = 5;
+                    int agilidad = 5;
+                    int inteligencia = 5;
+                    int voluntad = 5;
+
+                    switch (raza) {
+                        case HUMANO:
+                            inteligencia += 2;
+                            break;
+                        case ELFO:
+                            agilidad += 2;
+                            break;
+                        case ENANO:
+                            voluntad += 2;
+                            break;
+                        case ORCO:
+                            fuerza += 2;
+                            break;
+                    }
+
+                    // Crea una instancia de la clase de personaje específica según la profesión elegida.
+                    Personaje nuevoPersonaje = null;
+                    switch (rol) {
+                        case GUERRERO:
+                            nuevoPersonaje = new Guerrero(nombre, raza, fuerza, agilidad, inteligencia, voluntad);
+                            break;
+                        case MAGO:
+                            nuevoPersonaje = new Mago(nombre, raza, fuerza, agilidad, inteligencia, voluntad);
+                            break;
+                        case LADRON:
+                            nuevoPersonaje = new Ladron(nombre, raza, fuerza, agilidad, inteligencia, voluntad);
+                            break;
+                        case SACERDOTE:
+                            nuevoPersonaje = new Sacerdote(nombre, raza, fuerza, agilidad, inteligencia, voluntad);
+                            break;
+                    }
+
+                    if (nuevoPersonaje != null) {
+                        grupoDePersonajes.add(nuevoPersonaje);
+                        System.out.println("\n¡Personaje creado y sumado al grupo!");
+                    }
+                    break;
+
+                case "2":
+                    if (grupoDePersonajes.isEmpty()) {
+                        System.out.println("Tu grupo está vacío, crea un personaje primero.");
+                    } else {
+                        System.out.println("\n--- GRUPO ACTUAL ---");
+                        for (int i = 0; i < grupoDePersonajes.size(); i++) {
+                            Personaje personaje = grupoDePersonajes.get(i);
+                            System.out.println((i + 1) + ". " + personaje.getNombre() + " (" + personaje.getRaza() + " " + personaje.getRol() + ")");
+                        }
+                    }
+                    break;
+
+                case "3":
+                    if (grupoDePersonajes.isEmpty()) {
+                        System.out.println("Tu grupo está vacío, crea un personaje primero");
+                        break;
+                    }
+
+                    System.out.println("\n--- SELECCIONAR PERSONAJE ---");
+                    for (int i = 0; i < grupoDePersonajes.size(); i++) {
+                        System.out.println((i + 1) + ". " + grupoDePersonajes.get(i).getNombre());
+                    }
+                    System.out.print("Ingresa el número del grupo de personajes que quieres seleccionar: ");
+                    try {
+                        int index = Integer.parseInt(scanner.nextLine()) - 1;
+                        if (index >= 0 && index < grupoDePersonajes.size()) {
+                            Personaje personajeSeleccionado = grupoDePersonajes.get(index);
+
+                            while (true) {
+                                System.out.println("\n--- Qué quieres que haga " + personajeSeleccionado.getNombre() + " ? ---");
+                                System.out.println("Acciones: golpear, disparar, pensar, convencer, " + personajeSeleccionado.getAccionEspecial() + ", guardar, salir");
+                                System.out.print("Ingresa tu elección: ");
+                                String action = scanner.nextLine().toLowerCase();
+
+                                switch (action) {
+                                    case "golpear":
+                                        personajeSeleccionado.golpear();
+                                        break;
+                                    case "disparar":
+                                        personajeSeleccionado.disparar();
+                                        break;
+                                    case "pensar":
+                                        personajeSeleccionado.pensar();
+                                        break;
+                                    case "convencer":
+                                        personajeSeleccionado.convencer();
+                                        break;
+                                    case "bloquear":
+                                    case "conjurar":
+                                    case "esconderse":
+                                    case "rezar":
+                                        personajeSeleccionado.hacerAccionEspecial();
+                                        break;
+                                    case "guardar":
+                                        DataHandler.guardarTodosLosPersonajes(grupoDePersonajes, "personajes.txt");
+                                        break;
+                                    case "salir":
+                                        break;
+                                    default:
+                                        System.out.println("Entrada inválida. Por favor seleccione una de la lista.");
+                                        break;
+                                }
+                                if (action.equals("salir")) {
+                                    break;
+                                }
+                            }
+                        } else {
+                            System.out.println("Número inválido.");
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("Entrada inválida, elija un número de la lista.");
+                    }
+                    break;
+
+                case "4":
+                    DataHandler.guardarTodosLosPersonajes(grupoDePersonajes, "personajes.txt");
+                    break;
+
+                case "5":
+                    grupoDePersonajes = DataHandler.cargarTodosLosPersonajes("personajes.txt");
+                    System.out.println("Personajes cargados desde el archivo.");
+                    break;
+
+                case "6":
+                    System.out.println("Saliendo del programa. ¡Adiós!");
+                    scanner.close();
+                    return;
+
+                default:
+                    System.out.println("Entrada inválida, por favor seleccione una de la lista.");
+                    break;
             }
-        }
-
-        // Prompt ROL
-        Rol rol = null;
-        while (rol == null) {
-            System.out.println("Elije un rol (Guerrero, Mago, Ladrón, Sacerdote): ");
-            String rolIngresado = scanner.nextLine().toUpperCase();
-            try {
-                rol = Rol.valueOf(rolIngresado);
-            } catch (IllegalArgumentException e) {
-                System.out.println("Rol inválido. Por favor, seleccione uno de la lista.");
-            }
-        }
-
-        // Inicializamos los atributos básicos para el personaje
-        int fuerza = 5;
-        int agilidad = 5;
-        int inteligencia = 5;
-        int voluntad = 5;
-
-        // Aplicamos los bonificadores raciales
-        switch (raza) {
-            case HUMANO:
-                inteligencia += 2;
-                break;
-            case ELFO:
-                agilidad += 2;
-                break;
-            case ENANO:
-                voluntad += 2;
-                break;
-            case ORCO:
-                fuerza += 2;
-                break;
-        }
-        // Crea una instancia de la clase de personaje específica según la profesión elegida.
-        Personaje nuevoPersonaje = null;
-        switch (rol) {
-            case GUERRERO:
-                nuevoPersonaje = new Guerrero(nombre, raza, fuerza, agilidad, inteligencia, voluntad);
-                break;
-            case MAGO:
-                nuevoPersonaje = new Mago(nombre, raza, fuerza, agilidad, inteligencia, voluntad);
-                break;
-            case LADRON:
-                nuevoPersonaje = new Ladron(nombre, raza, fuerza, agilidad, inteligencia, voluntad);
-                break;
-            case SACERDOTE:
-                nuevoPersonaje = new Sacerdote(nombre, raza, fuerza, agilidad, inteligencia, voluntad);
-                break;
-        }
-        if (nuevoPersonaje != null) {
-            System.out.println("Nombre: " + nuevoPersonaje.getNombre());
-            System.out.println("Raza: " + nuevoPersonaje.getRaza());
-            System.out.println("Rol: " + nuevoPersonaje.getRol());
-            System.out.println("Fuerza: " + nuevoPersonaje.getFuerza());
-            System.out.println("Agilidad: " + nuevoPersonaje.getAgilidad());
-            System.out.println("Inteligencia: " + nuevoPersonaje.getInteligencia());
-            System.out.println("Voluntad: " + nuevoPersonaje.getVoluntad());
-
-            while (true) {
-                System.out.println("\n--- ¿Qué quieres hacer en tu turno? ---");
-                System.out.println("Acciones: golpear, disparar, pensar, convencer, " + nuevoPersonaje.getAccionEspecial() + ", guardar, salir");
-                System.out.print("Escribe tu elección: ");
-                String accion = scanner.nextLine().toLowerCase();
-
-                // Declaración "switch" para elegir una acción.
-                switch (accion) {
-                    case "golpear":
-                        nuevoPersonaje.golpear();
-                        break;
-                    case "disparar":
-                        nuevoPersonaje.disparar();
-                        break;
-                    case "pensar":
-                        nuevoPersonaje.pensar();
-                        break;
-                    case "convencer":
-                        nuevoPersonaje.convencer();
-                        break;
-                    // La acción especial se llama directamente a través de una llamada de mét.odo polimórfico.
-                    case "bloquear":
-                    case "conjurar":
-                    case "esconderse":
-                    case "rezar":
-                        nuevoPersonaje.hacerAccionEspecial();
-                        break;
-                    case "guardar":
-                        DataHandler.guardarPersonaje(nuevoPersonaje, "personajes.txt");
-                        break;
-                    case "salir":
-                        System.out.println("Programa terminado ¡Adiós!");
-                        scanner.close();
-                        return; // Sale del method Main
-                    default:
-                        System.out.println("Acción inválida, por favor seleccione una de la lista.");
-                        break;
-                }
-            }
-        }
-    }
-}
-class DataHandler {
-    public static void guardarPersonaje(Personaje personaje, String fileName) {
-        try (PrintWriter writer = new PrintWriter(new FileWriter(fileName, true))) {
-            writer.println(personaje.getNombre() + "," + personaje.getRaza().toString() + "," + personaje.getRol().toString() + "," +
-                    personaje.getFuerza() + "," + personaje.getAgilidad() + "," + personaje.getInteligencia() + "," + personaje.getVoluntad());
-            System.out.println("Personaje guardado: " + personaje.getNombre());
-        } catch (IOException e) {
-            System.out.println("No se puedo guardar el personaje.");
-        }
-    }
-
-    public static void loadCharacters(String fileName) {
-        File file = new File(fileName);
-        try (Scanner scanner = new Scanner(file)) {
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                String[] parts = line.split(",");
-                // Check if the line has all the expected data parts.
-                if (parts.length == 7) {
-                    String nombre = parts[0];
-                    Raza raza = Raza.valueOf(parts[1]);
-                    Rol rol = Rol.valueOf(parts[2]);
-                    int fuerza = Integer.parseInt(parts[3]);
-                    int agilidad = Integer.parseInt(parts[4]);
-                    int inteligencia = Integer.parseInt(parts[5]);
-                    int voluntad = Integer.parseInt(parts[6]);
-
-                    System.out.println("Loaded character: " + nombre + " - " + raza + " - " + rol +
-                            " (Str: " + fuerza + ", Agi: " + agilidad + ", Int: " + inteligencia + ", Will: " + voluntad + ")");
-                }
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found: " + fileName);
         }
     }
 }
